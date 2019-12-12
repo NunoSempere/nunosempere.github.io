ee4ba42fc8ddaba41f551fb543addebaf6fb221c
Nuño Sempere <nuno.sempere@gmail.com>
2019-12-12 11:19:45 +0100

    Update geocentric.R

--- maths-prog/geocentric.R
+++ maths-prog/geocentric.R
@@ -7,18 +7,16 @@ sun = function(t){
   return(c(x,y))
 }
 
-sun(t)
 d = list()
 d$x = c()
 d$y = c()
-
 for(t in c(1:2000)){
   p_sun = sun(t)
   d$x=c(d$x,p_sun[1])
   d$y=c(d$y, p_sun[2])
 }
-
 d= as.data.frame(d)
+
 blank = theme_void() + theme(legend.position="none")
 ggplot(data = d)+
   geom_point(aes(x=x, y=y), size = 0.0001)+
